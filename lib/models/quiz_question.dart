cd317b053d94f5132ed338d5605133588ddf04a6
realbro <realbro.noh@gmail.com>
2023-12-16 21:27:31 +0900

    quiz page with dynamic button

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -3,4 +3,10 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  List<String> getShuffeledAnswers() {
+    final shuffledAnswers = List.of(answers);
+    shuffledAnswers.shuffle();
+    return shuffledAnswers;
+  }
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/answer_button.dart';
+import 'package:quiz/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,8 +12,38 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  int currentQuestionIndex = 0;
+
+  void moveNextQuestion() {
+    setState(() => currentQuestionIndex += 1);
+  }
+
   @override
   Widget build(context) {
-    return const Text("QuestionsScreen");
+    final currentQuestion = questions[currentQuestionIndex];
+    return SizedBox(
+      width: double.infinity, // as wide as possible
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffeledAnswers().map(
+                  (answer) => AnswerButton(
+                    answerText: answer,
+                    onTap: moveNextQuestion,
+                  ),
+                ),
+          ],
+        ),
+      ),
+    );
   }
 }
